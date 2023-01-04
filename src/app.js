5f8e40825f869b3be79ea790a2c84cb25e4a2fc0
tomomicomber <121703592+tomomicomber@users.noreply.github.com>
2023-01-05 07:48:04 +1000

    added a new function (displayForecast) and moved some elements from HTML to Javascript

--- src/app.js
+++ src/app.js
@@ -21,6 +21,26 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+<div class="col-2">
+  <div class="weather-forecast-date">${day}</div>
+  <img src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png" alt="">
+ <div class="weather-forecast-temperatures"> 
+  <span class="weather-forecast-max">18°</span> <span class="weather-forecast-min">12°</span></div>
+</div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temp");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -85,3 +105,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
